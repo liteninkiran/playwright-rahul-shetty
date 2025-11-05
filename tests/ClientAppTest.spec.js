4f7ef0ae6825688fae7a6fbae993c596302851c9
Kiran Anand <liteninkiran@hotmail.com>
2025-11-05 14:31:56 +0000

    028 - Write the Script to dynamically find the product to buy from list of products

--- tests/ClientAppTest.spec.js
+++ tests/ClientAppTest.spec.js
@@ -18,3 +18,31 @@ test('Test another homepage loads when correct credentials are entered', async (
     // Assert
     await expect(cards).not.toHaveCount(0);
 });
+
+test.only('Test basket', async ({
+    page,
+}) => {
+    // Arrange
+    const productName = 'ZARA COAT 3';
+    const cards = page.locator('.card-body b');
+    const url = `${BASE_URL}/client/auth/login/`;
+    const products = page.locator('.card-body');
+    await page.goto(url);
+
+    // Act
+    await page.locator('#userEmail').fill('test551254@gmail.com');
+    await page.locator('#userPassword').fill('Rahulshetty1!');
+    await page.locator('#login').click();
+
+    const count = await products.count();
+
+    for (const i = 0; i < count; i++) {
+        if (await products.nth(i).locator('b').textContent() === productName) {
+            await products.nth(i).locator('text=Add To Cart').click();
+            break;
+        }
+    }
+
+    // Assert
+    
+});
