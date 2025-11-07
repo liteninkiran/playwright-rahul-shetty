912704397af97ca746db722b2a1a0aed30e4f298
Kiran Anand <liteninkiran@hotmail.com>
2025-11-07 09:43:38 +0000

    037 - Filtering elements with GetByRole, GetByText and perform chaining methods in step

--- tests/ProtractorTest.spec.js
+++ tests/ProtractorTest.spec.js
@@ -7,5 +7,13 @@ test.only('Playwright Special Locators...', async ({ page }) => {
     await page.getByLabel('Check me out if you Love IceCreams!').click(); // or .check()
     await page.getByLabel('Employed').click(); // or .check()
     await page.getByLabel('Gender').selectOption('Female');
+
+    await page.getByPlaceholder('Password').fill('abc123');
+    await page.getByRole('button', { name: 'Submit' }).click();
+    const isVisible = await page.getByText('Success! The Form has been submitted successfully!.').isVisible();
+    await page.getByRole('link', { name: 'Shop' }).click();
     await page.pause();
+    await page.locator('app-card').filter({
+        hasText: 'Nokia Edge',
+    }).getByRole('button').click();
 });
